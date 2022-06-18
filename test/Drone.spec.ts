140c6b875062f6cab85ba63bf771f365448cfb47
Vitor Diego <vitordiegossousa@gmail.com>
2022-06-18 20:25:29 -0300

    Chore: initial commit and setup

---
+++ test/Drone.spec.ts
@@ -0,0 +1,4 @@
+describe('Should handle Drones', () => {
+  test('Should create a new Drone when input information is provided', () => {});
+  test('Should throw an exception when input information is not provided', () => {});
+});
