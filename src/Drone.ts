5f83cfa10b8646b332f53bf873deec60ceff71b9
Vitor Diego <vitordiegossousa@gmail.com>
2022-06-18 21:51:47 -0300

    Refactor: set Drone default class

---
+++ src/Drone.ts
@@ -0,0 +1,11 @@
+export default class Drone {
+  constructor(private maxWeight: number, private name: string) {}
+
+  get getName() {
+    return this.name;
+  }
+
+  get getMaxWeight() {
+    return this.maxWeight;
+  }
+}
--- test/Drone.spec.ts
+++ test/Drone.spec.ts
@@ -1,12 +1,11 @@
-import { Drone } from '../src/index';
+import Drone from '../src/Drone';
 
-describe('Should handle Drones', () => {
-  test('Should create a new Drone when input information is provided', () => {
+describe('Should handle Drone instances', () => {
+  test('Should instantiate a new Drone', () => {
     const drone = new Drone(300, 'drone one');
 
     expect(drone).toBeDefined();
     expect(drone.getMaxWeight).toBe(300);
     expect(drone.getName).toBe('drone one');
   });
-  test('Should throw an exception when input information is not provided', () => {});
 });
